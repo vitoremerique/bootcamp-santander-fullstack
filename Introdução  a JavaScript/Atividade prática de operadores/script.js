ca745f3403012034d24f8856ecaae4a86f054d37
VitorEmerique <vitort.emerique@hotmail.com>
2022-06-08 14:45:04 -0300

    Atividade prática com operadores JS

---
+++ Introdução  a JavaScript/Atividade prática de operadores/script.js
@@ -0,0 +1,9 @@
+function soma(a,b){
+    var sum = a+b;
+    console.log(a+b);
+    (a===b)? console.log("São números iguais"):console.log("São números diferentes");
+    (sum>10 && sum<20)? console.log("É maior que 10 e menor que 20"):console.log();
+
+}
+
+soma(6,6);
