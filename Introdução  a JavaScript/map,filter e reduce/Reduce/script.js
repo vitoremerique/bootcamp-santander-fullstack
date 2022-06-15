61fd1fe315baffec5a2a110e9fe12e1907d59e5f
VitorEmerique <vitort.emerique@hotmail.com>
2022-06-14 21:34:04 -0300

    Prática com Reduce

--- Introdução  a JavaScript/map,filter e reduce/Reduce/script.js
+++ Introdução  a JavaScript/map,filter e reduce/Reduce/script.js
@@ -1,11 +1,25 @@
-function somaNumeros(arr){
-    return arr.reduce(function(prev,current){
+const lista=[
+    {
+        name:'Sabão em pó',
+        preco:30
+    },
+    {
+        name:'Cereal',
+        preco:12
+    },
+    {
+        name:'toalha',
+        preco:30
+    }
+]
+const SaldoDisponivel = 100
+function calculaSaldo(saldodisponivel,lista){
+    return lista.reduce(function(prev,current,index){
+        console.log('Rodada',index+1)
         console.log({prev})
         console.log({current})
-        return prev+current;
-    })
+        return prev-current.preco
+    },saldodisponivel)
 }
 
-const nums = [1,2,3,4,5]
-
-console.log('Soma numeros',somaNumeros(nums))
+console.log(calculaSaldo(SaldoDisponivel,lista))
