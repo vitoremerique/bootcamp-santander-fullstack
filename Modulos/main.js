8ba61b43df62aa96463af5f53acac0eee3c776ec
VitorEmerique <vitort.emerique@hotmail.com>
2022-07-04 21:34:12 -0300

    'Modulos'

---
+++ Modulos/main.js
@@ -0,0 +1,5 @@
+import { mostraIdade,mostraCidade,mostraHobby } from "./funcoes.mjs";
+
+console.log(mostraIdade('Vitor',21));
+console.log(mostraCidade('João','Santarém - Pa'))
+console.log(mostraHobby('Carlos','Jogar Video game'))
