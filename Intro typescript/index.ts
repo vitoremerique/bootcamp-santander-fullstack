7abf0c606a539ab69f199046501190ee347e56e0
VitorEmerique <vitort.emerique@hotmail.com>
2022-07-06 16:00:24 -0300

    reconhecendo input com typescript

--- Intro typescript/index.ts
+++ Intro typescript/index.ts
@@ -1,32 +1,7 @@
-interface IAnimal{
-    nome : string;
-    tipo: 'Terrestre' | 'Voador';
-    domestico:boolean;
-}
+const input = document.getElementById('input')as HTMLInputElement;
 
-interface IFelino extends IAnimal{
-    Pelos: boolean;
-    VisaoNoturna:boolean;
-}
-
-interface ICanino extends IAnimal{
- porte : 'pequeno' | 'medio' | 'grande'
-}
-
-type IDomestico = IFelino | ICanino
-
-const felino: IFelino = {
-    nome:'Macaco',
-    tipo:'Terrestre',
-    Pelos:true,
-    VisaoNoturna:true,
-    domestico:false
-} 
-
-const animal: IDomestico = {
-    domestico:true,
-    nome: 'cachorro',
-    porte:'medio',
-    VisaoNoturna:true,
-    tipo:'Terrestre'
-}
+input.addEventListener('input',(event)=>{
+ console.log('digitei');
+ const i = event.currentTarget as HTMLInputElement;
+ console.log(i.value)
+});
