759824821005c234a41e7cbe06ce17bae8d85c3e
VitorEmerique <vitort.emerique@hotmail.com>
2022-07-06 15:46:13 -0300

    Typescript Interfaces/Types

--- Intro typescript/index.ts
+++ Intro typescript/index.ts
@@ -1,5 +1,32 @@
-function soma (a: number,b: number){
-    return a+b
+interface IAnimal{
+    nome : string;
+    tipo: 'Terrestre' | 'Voador';
+    domestico:boolean;
 }
 
-console.log(soma(2,3));
+interface IFelino extends IAnimal{
+    Pelos: boolean;
+    VisaoNoturna:boolean;
+}
+
+interface ICanino extends IAnimal{
+ porte : 'pequeno' | 'medio' | 'grande'
+}
+
+type IDomestico = IFelino | ICanino
+
+const felino: IFelino = {
+    nome:'Macaco',
+    tipo:'Terrestre',
+    Pelos:true,
+    VisaoNoturna:true,
+    domestico:false
+} 
+
+const animal: IDomestico = {
+    domestico:true,
+    nome: 'cachorro',
+    porte:'medio',
+    VisaoNoturna:true,
+    tipo:'Terrestre'
+}
