713d951f2af1e5cce01063fb3fd4fd4201397332
VitorEmerique <vitort.emerique@hotmail.com>
2022-07-20 17:51:33 -0300

    'Final'

--- Angular SPA/spaangular/src/app/home/home.component.ts
+++ Angular SPA/spaangular/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { alunos } from '../model/alunos'
 
 @Component({
   selector: 'spa-home',
@@ -9,7 +10,32 @@ export class HomeComponent implements OnInit {
 
   constructor() { }
 
+  valor!: number;
+  exibeTabela:  boolean = false;
+
+  listaAlunos: alunos[] = [
+    { nome: 'Camila', idade: 27, email: 'camila.taruma@gmail.com', curso: 'Ciencia da Computacao' },
+    { nome: 'Teste', idade: 25, email: 'Teste@gmail.com', curso: 'Analise e desenvolvimento de sistemas' },
+    { nome: 'Teste 2', idade: 31, email: 'Teste2@gmail.com', curso: 'Engenharia da computacao' },
+    { nome: 'Teste 3', idade: 45, email: 'Teste3@gmail.com', curso: 'Sistemas da informacao' }
+  ];
+
+  displayedColumns: string[] = ['Nome', 'Idade', 'E-mail', 'Curso'];
+
   ngOnInit(): void {
+    this.valor = 5;
+  }
+
+  mudarValor() {
+    this.valor++;
+  }
+
+  reiniciarValor() {
+    this.valor = 0;
+  }
+
+  exibirTabela(){
+    this.exibeTabela = true;
   }
 
 }
